8a774b3641210fc9ac1b1f8824bc2fcce9626a6b
alex.incerti <alex.incerti@Inzolia.adcgroup.domain>
2017-12-07 20:04:17 +0100

    overtime metrics and richer events

--- backend/src/main/java/com/group14/findeyourfriend/Simulator.java
+++ backend/src/main/java/com/group14/findeyourfriend/Simulator.java
@@ -1,14 +1,14 @@
 package com.group14.findeyourfriend;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Queue;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 import com.group14.findeyourfriend.debug.DebugLog;
 import com.group14.findeyourfriend.simulation.Simulation;
@@ -34,28 +34,31 @@ public class Simulator {
 		// simulator.start();
 	}
 
-	@Scheduled(fixedDelay = 15000)
+	@PostConstruct
 	public void start() {
-		try {
-			boolean runSR = false;
-			InputStream eventStream;
-			InputStream paramStream;
-			if(!runSR){
-				eventStream = Simulator.class.getClassLoader().getResourceAsStream("events.txt");
-				paramStream = Simulator.class.getClassLoader().getResourceAsStream("params.txt");
-			} else{
-				eventStream = Simulator.class.getClassLoader().getResourceAsStream("events.txt");
-				paramStream = Simulator.class.getClassLoader().getResourceAsStream("SRparams.txt");
+		new Thread(() -> {
+			try {
+				Thread.sleep(5000);
+				boolean runSR = false;
+				InputStream eventStream;
+				InputStream paramStream;
+				if (!runSR) {
+					eventStream = Simulator.class.getClassLoader().getResourceAsStream("events.txt");
+					paramStream = Simulator.class.getClassLoader().getResourceAsStream("params.txt");
+				} else {
+					eventStream = Simulator.class.getClassLoader().getResourceAsStream("events.txt");
+					paramStream = Simulator.class.getClassLoader().getResourceAsStream("SRparams.txt");
+				}
+				Queue<Event> events = EventParser.parse(eventStream);
+				Queue<Parameters> params = ParameterParser.parse(paramStream, runSR);
+				simulation.add(calculator::calculate);
+				simulation.addEventListener(calculator::braceletEvent);
+				simulation.init(events, runSR, 3600000);// Simulate 1 hour in ms
+				simulation.run(params.poll());
+				// Chart.main(new String[0]);// Show chart
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			Queue<Event> events = EventParser.parse(eventStream);
-			Queue<Parameters> params = ParameterParser.parse(paramStream, runSR);
-			simulation.add(calculator::calculate);
-			simulation.addEventListener(calculator::braceletEvent);
-			simulation.init(events, runSR, 86400000);// Simulate 1 days in ms
-			simulation.run(params.poll());
-			// Chart.main(new String[0]);// Show chart
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		}).start();
 	}
 }
