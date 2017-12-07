8d89afa74d8676aa6dc83514ccd7030baf5b356b
Salik Lennert Pedersen <saliklp@gmail.com>
2017-12-07 10:45:48 +0100

    Only first 30 db entries that satisfy recent time threshold

--- backend/src/main/java/com/group14/findeyourfriend/chart/Chart.java
+++ backend/src/main/java/com/group14/findeyourfriend/chart/Chart.java
@@ -43,25 +43,8 @@ public class Chart extends Application {
             seriesArrayList.add(series);
 			lineChart.setCreateSymbols(false);
             lineChart.getData().add(series);
-
         }
-
-//        series.getData().add(new XYChart.Data(1, 23));
-//        series.getData().add(new XYChart.Data(2, 14));
-//        series.getData().add(new XYChart.Data(3, 15));
-//        series.getData().add(new XYChart.Data(4, 24));
-//        series.getData().add(new XYChart.Data(5, 34));
-//        series.getData().add(new XYChart.Data(6, 36));
-//        series.getData().add(new XYChart.Data(7, 22));
-//        series.getData().add(new XYChart.Data(8, 45));
-//        series.getData().add(new XYChart.Data(9, 43));
-//        series.getData().add(new XYChart.Data(10, 17));
-//        series.getData().add(new XYChart.Data(11, 29));
-//        series.getData().add(new XYChart.Data(12, 25));
-
         Scene scene  = new Scene(lineChart,800,600);
-
-
 		stage.setScene(scene);
 		stage.show();
 	}
